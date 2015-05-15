3ba917e995c30642343ff8751be6e219781e968e
tjcoffey4 <thomas.coffey@utexas.edu>
2015-05-15 13:12:02 -0500

    added new post and post index fuctions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  resources :posts
+  root "posts#index"
+end
+
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +64,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
